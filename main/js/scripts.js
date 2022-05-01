60ba8cfd02d49f1e7d78955db8acd4adf9f164bd
horizD <he1236@ajou.ac.kr>
2022-05-01 19:31:57 +0900

    pose 모델 적용중

--- main/js/scripts.js
+++ main/js/scripts.js
@@ -1,105 +1,44 @@
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-
-canvas.width = window.innerWidth - 650;
-canvas.height = window.innerHeight - 650;
-
-var BulletColors = [
-  "#fb4c00",
-  "#fc0000",
-  "#fc0000",
-  "#fc0000",
-  "#b00000",
-  "#960000",
-  "#620000",
-  "#590000",
-  "#340000",
-  "#190000",
-  "#000000",
-];
-
-var audio_shoot = new Audio("./assets/sound/silencerShoot1_p.mp3");
-var audio_reload = new Audio("./assets/sound/reload0_p.wav");
-
-var bullet = {
-  x: 10,
-  y: 200,
-  width: 30,
-  height: 30,
-  reload() {
-    audio_reload.play();
-  },
-  shoot() {
-    audio_shoot.play();
-    ctx.arc(600, 200, 10, 0, 2 * Math.PI);
-    ctx.stroke();
-    ctx.fillStyle = BulletColors[0];
-    ctx.fill();
-    for (i = 1; i < BulletColors.length - 1; i++) {
-      (function (x) {
-        setTimeout(function () {
-          ctx.fillStyle = BulletColors[x];
-          ctx.fill();
-        }, 50 * x);
-      })(i);
-    }
-    canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
-  },
-};
 //////////////////////////////
 //Main HTML Element
 
-function delete_startTag() {
-  // var header_title = document.getElementById("header_title");
-  // var title_main = document.getElementById("title_main");
-  // var title_sub = document.getElementById("title_sub");
-  // var button_start = document.getElementById("button_start");
-  // header_title.removeChild(title_main);
-  // header_title.removeChild(title_sub);
-  // header_title.removeChild(button_start);
-}
-
-// function sleep(delay) {
-//   console.log("대기중.................");
-//   var start = new Date().getTime();
-//   while (new Date().getTime() < start + delay);
-// }
-
-//////////////////////////////
-
 // More API functions here:
-// https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/image
+// https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/pose
 
+//////////////////////////////////////
 // the link to your model provided by Teachable Machine export panel
-const URL = "../my_model/";
+const URL = "./my_model/";
+let model, webcam, ctx, labelContainer, maxPredictions;
 
-let model, webcam, labelContainer, maxPredictions;
+////
 
-// Load the image model and setup the webcam
+/////
 async function init() {
   $("#title_main").fadeOut("slow");
   $("#title_sub").fadeOut("slow");
   $("#button_start").fadeOut("slow");
 
-  delete_startTag();
   const modelURL = URL + "model.json";
   const metadataURL = URL + "metadata.json";
+
   // load the model and metadata
   // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
-  // or files from your local hard drive
-  // Note: the pose library adds "tmImage" object to your window (window.tmImage)
-  model = await tmImage.load(modelURL, metadataURL);
+  // Note: the pose library adds a tmPose object to your window (window.tmPose)
+  model = await tmPose.load(modelURL, metadataURL);
   maxPredictions = model.getTotalClasses();
 
   // Convenience function to setup a webcam
+  const size = 200;
   const flip = true; // whether to flip the webcam
-  webcam = new tmImage.Webcam(300, 300, flip); // width, height, flip
+  webcam = new tmPose.Webcam(size, size, flip); // width, height, flip
   await webcam.setup(); // request access to the webcam
   await webcam.play();
   window.requestAnimationFrame(loop);
 
-  // append elements to the DOM
-  document.getElementById("webcam-container").appendChild(webcam.canvas);
+  // append/get elements to the DOM
+  const canvas = document.getElementById("canvas_ML");
+  canvas.width = size;
+  canvas.height = size;
+  ctx = canvas.getContext("2d");
   labelContainer = document.getElementById("label-container");
   for (let i = 0; i < maxPredictions; i++) {
     // and class labels
@@ -107,25 +46,95 @@ async function init() {
   }
 }
 
-async function loop() {
+async function loop(timestamp) {
   webcam.update(); // update the webcam frame
   await predict();
   window.requestAnimationFrame(loop);
 }
 
-// run the webcam image through the image model
 async function predict() {
-  // predict can take in an image, video or canvas html element
-  const prediction = await model.predict(webcam.canvas);
-  if (prediction[2].probability.toFixed(2) >= 0.4) {
-    bullet.reload();
-    canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
-    labelContainer.childNodes[0].innerHTML = "장전중..";
-  } else if (prediction[1].probability.toFixed(2) == 1) {
-    bullet.shoot();
-    labelContainer.childNodes[0].innerHTML = "땅!";
-  } else if (prediction[0].probability.toFixed(2) >= 0.1) {
-    canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+  // Prediction #1: run input through posenet
+  // estimatePose can take in an image, video or canvas html element
+  const { pose, posenetOutput } = await model.estimatePose(webcam.canvas);
+  // Prediction 2: run input through teachable machine classification model
+  const prediction = await model.predict(posenetOutput);
+
+  if (prediction[0].probability.toFixed(2) > 0.5) {
     labelContainer.childNodes[0].innerHTML = "대기중";
+  } else if (prediction[1].probability.toFixed(2) > 0.5) {
+    labelContainer.childNodes[0].innerHTML = "땅!";
+    shoot();
+  } else {
+    labelContainer.childNodes[0].innerHTML = "장전중..";
+    reload();
+  }
+  // for (let i = 0; i < maxPredictions; i++) {
+  //   const classPrediction =
+  //     prediction[i].className + ": " + prediction[i].probability.toFixed(2);
+  //   labelContainer.childNodes[i].innerHTML = classPrediction;
+  // }
+
+  // finally draw the poses
+  drawPose(pose);
+}
+
+////
+
+function reload() {
+  var audio_reload = new Audio("./assets/sound/reload0_p.wav");
+
+  audio_reload.play();
+}
+
+function shoot() {
+  var audio_shoot = new Audio("./assets/sound/silencerShoot1_p.mp3");
+
+  var BulletColors = [
+    "#fb4c00",
+    "#fc0000",
+    "#fc0000",
+    "#fc0000",
+    "#b00000",
+    "#960000",
+    "#620000",
+    "#590000",
+    "#340000",
+    "#190000",
+    "#000000",
+  ];
+
+  var canvas_bullet = document.getElementById("canvas_bullet");
+  var ctx_bullet = canvas_bullet.getContext("2d");
+
+  canvas_bullet.width = window.innerWidth - 800;
+  canvas_bullet.height = window.innerHeight - 800;
+
+  audio_shoot.play();
+  ctx_bullet.arc(600, 200, 10, 0, 2 * Math.PI);
+  ctx_bullet.stroke();
+  ctx_bullet.fillStyle = BulletColors[0];
+  ctx_bullet.fill();
+  for (i = 1; i < BulletColors.length - 1; i++) {
+    (function (x) {
+      setTimeout(function () {
+        ctx_bullet.fillStyle = BulletColors[x];
+        ctx_bullet.fill();
+      }, 50 * x);
+    })(i);
+  }
+  canvas_bullet
+    .getContext("2d")
+    .clearRect(0, 0, canvas_bullet.width, canvas_bullet.height);
+}
+
+function drawPose(pose) {
+  if (webcam.canvas) {
+    ctx.drawImage(webcam.canvas, 0, 0);
+    // draw the keypoints and skeleton
+    if (pose) {
+      const minPartConfidence = 0.5;
+      tmPose.drawKeypoints(pose.keypoints, minPartConfidence, ctx);
+      tmPose.drawSkeleton(pose.keypoints, minPartConfidence, ctx);
+    }
   }
 }
