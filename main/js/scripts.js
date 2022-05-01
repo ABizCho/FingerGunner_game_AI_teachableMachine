8130d4d4a1963577f626438c7574d7ecd3c909a4
horizD <he1236@ajou.ac.kr>
2022-05-01 17:49:26 +0900

    img 기반 학습 최종 : pose기반으로 수정 대기중

--- main/js/scripts.js
+++ main/js/scripts.js
@@ -18,12 +18,19 @@ var BulletColors = [
   "#000000",
 ];
 
+var audio_shoot = new Audio("./assets/sound/silencerShoot1_p.mp3");
+var audio_reload = new Audio("./assets/sound/reload0_p.wav");
+
 var bullet = {
   x: 10,
   y: 200,
   width: 30,
   height: 30,
-  draw() {
+  reload() {
+    audio_reload.play();
+  },
+  shoot() {
+    audio_shoot.play();
     ctx.arc(600, 200, 10, 0, 2 * Math.PI);
     ctx.stroke();
     ctx.fillStyle = BulletColors[0];
@@ -36,20 +43,28 @@ var bullet = {
         }, 50 * x);
       })(i);
     }
+    canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
   },
 };
 //////////////////////////////
 //Main HTML Element
 
 function delete_startTag() {
-  var title_id = getElementById("title_main");
-  var title_sub = getElementById("title_sub");
-  var button_start = getElementById("button_start");
-
-  //   title_id.remove();
-  //   title_sub.remove();
-  //   button_start.remove();
+  // var header_title = document.getElementById("header_title");
+  // var title_main = document.getElementById("title_main");
+  // var title_sub = document.getElementById("title_sub");
+  // var button_start = document.getElementById("button_start");
+  // header_title.removeChild(title_main);
+  // header_title.removeChild(title_sub);
+  // header_title.removeChild(button_start);
 }
+
+// function sleep(delay) {
+//   console.log("대기중.................");
+//   var start = new Date().getTime();
+//   while (new Date().getTime() < start + delay);
+// }
+
 //////////////////////////////
 
 // More API functions here:
@@ -62,9 +77,13 @@ let model, webcam, labelContainer, maxPredictions;
 
 // Load the image model and setup the webcam
 async function init() {
+  $("#title_main").fadeOut("slow");
+  $("#title_sub").fadeOut("slow");
+  $("#button_start").fadeOut("slow");
+
+  delete_startTag();
   const modelURL = URL + "model.json";
   const metadataURL = URL + "metadata.json";
-  bullet.draw();
   // load the model and metadata
   // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
   // or files from your local hard drive
@@ -99,12 +118,14 @@ async function predict() {
   // predict can take in an image, video or canvas html element
   const prediction = await model.predict(webcam.canvas);
   if (prediction[2].probability.toFixed(2) >= 0.4) {
+    bullet.reload();
+    canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
     labelContainer.childNodes[0].innerHTML = "장전중..";
-  } else if (prediction[0].probability.toFixed(2) >= 0.2) {
+  } else if (prediction[1].probability.toFixed(2) == 1) {
+    bullet.shoot();
+    labelContainer.childNodes[0].innerHTML = "땅!";
+  } else if (prediction[0].probability.toFixed(2) >= 0.1) {
     canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
     labelContainer.childNodes[0].innerHTML = "대기중";
-  } else if (prediction[1].probability.toFixed(2) >= 0.999) {
-    bullet.draw();
-    labelContainer.childNodes[0].innerHTML = "땅!";
   }
 }
