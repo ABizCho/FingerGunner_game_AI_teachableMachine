75ac76ddd89a6752d02f4482031e5225be59cb89
horizD <he1236@ajou.ac.kr>
2022-05-02 15:08:42 +0900

    shoot 발사흔 남지 않는상태 : function 작동도 전에 loop진행되는 오류로 보인다

--- main/js/scripts.js
+++ main/js/scripts.js
@@ -1,22 +1,58 @@
-//////////////////////////////
-//Main HTML Element
-
-// More API functions here:
-// https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/pose
-
-//////////////////////////////////////
-// the link to your model provided by Teachable Machine export panel
 const URL = "./my_model/";
 let model, webcam, ctx, labelContainer, maxPredictions;
 
-////
+let audio_reload, audio_shoot;
+audio_shoot = new Audio("./assets/sound/silencerShoot1_p.mp3");
+audio_reload = new Audio("./assets/sound/reload0_p.wav");
 
-/////
+function reload() {
+  audio_reload.play();
+}
+function shoot() {
+  audio_shoot.play();
+
+  let canvas_bullet, ctx_bullet, BulletColors;
+  canvas_bullet = document.getElementById("canvas_bullet");
+  ctx_bullet = canvas_bullet.getContext("2d");
+  BulletColors = [
+    "#fb4c00",
+    "#fc0000",
+    "#fc0000",
+    "#fc0000",
+    "#b00000",
+    "#960000",
+    "#620000",
+    "#590000",
+    "#340000",
+    "#190000",
+    "#000000",
+  ];
+  canvas_bullet.width = window.innerWidth - 500;
+  canvas_bullet.height = window.innerHeight - 500;
+
+  ctx_bullet.arc(600, 200, 10, 0, 2 * Math.PI);
+  ctx_bullet.stroke();
+  ctx_bullet.fillStyle = BulletColors[0];
+  ctx_bullet.fill();
+  for (i = 1; i < BulletColors.length - 1; i++) {
+    (function (x) {
+      setTimeout(function () {
+        ctx_bullet.fillStyle = BulletColors[x];
+        ctx_bullet.fill();
+      }, 50 * x);
+    })(i);
+  }
+  canvas_bullet
+    .getContext("2d")
+    .clearRect(0, 0, canvas_bullet.width, canvas_bullet.height);
+}
 async function init() {
   $("#title_main").fadeOut("slow");
   $("#title_sub").fadeOut("slow");
   $("#button_start").fadeOut("slow");
 
+  reload();
+  shoot();
   const modelURL = URL + "model.json";
   const metadataURL = URL + "metadata.json";
 
@@ -46,7 +82,7 @@ async function init() {
   }
 }
 
-async function loop(timestamp) {
+async function loop() {
   webcam.update(); // update the webcam frame
   await predict();
   window.requestAnimationFrame(loop);
@@ -59,12 +95,12 @@ async function predict() {
   // Prediction 2: run input through teachable machine classification model
   const prediction = await model.predict(posenetOutput);
 
-  if (prediction[0].probability.toFixed(2) > 0.5) {
+  if (prediction[0].probability.toFixed(2) > 0.7) {
     labelContainer.childNodes[0].innerHTML = "대기중";
   } else if (prediction[1].probability.toFixed(2) > 0.5) {
     labelContainer.childNodes[0].innerHTML = "땅!";
     shoot();
-  } else {
+  } else if (prediction[1].probability.toFixed(2) > 0.4) {
     labelContainer.childNodes[0].innerHTML = "장전중..";
     reload();
   }
@@ -79,53 +115,6 @@ async function predict() {
 }
 
 ////
-
-function reload() {
-  var audio_reload = new Audio("./assets/sound/reload0_p.wav");
-
-  audio_reload.play();
-}
-
-function shoot() {
-  var audio_shoot = new Audio("./assets/sound/silencerShoot1_p.mp3");
-
-  var BulletColors = [
-    "#fb4c00",
-    "#fc0000",
-    "#fc0000",
-    "#fc0000",
-    "#b00000",
-    "#960000",
-    "#620000",
-    "#590000",
-    "#340000",
-    "#190000",
-    "#000000",
-  ];
-
-  var canvas_bullet = document.getElementById("canvas_bullet");
-  var ctx_bullet = canvas_bullet.getContext("2d");
-
-  canvas_bullet.width = window.innerWidth - 800;
-  canvas_bullet.height = window.innerHeight - 800;
-
-  audio_shoot.play();
-  ctx_bullet.arc(600, 200, 10, 0, 2 * Math.PI);
-  ctx_bullet.stroke();
-  ctx_bullet.fillStyle = BulletColors[0];
-  ctx_bullet.fill();
-  for (i = 1; i < BulletColors.length - 1; i++) {
-    (function (x) {
-      setTimeout(function () {
-        ctx_bullet.fillStyle = BulletColors[x];
-        ctx_bullet.fill();
-      }, 50 * x);
-    })(i);
-  }
-  canvas_bullet
-    .getContext("2d")
-    .clearRect(0, 0, canvas_bullet.width, canvas_bullet.height);
-}
 
 function drawPose(pose) {
   if (webcam.canvas) {
